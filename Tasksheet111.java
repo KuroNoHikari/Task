d7732221b72fba0b863f18a82e543ed5fb5457be
KuroNoHikari <robert.delacruz.rdc27@gmail.com>
2024-12-03 14:14:47 +0000

    Task

---
+++ Tasksheet111.java
@@ -0,0 +1,13 @@
+class Tasksheet111 {
+    public static void main(String[] args) {
+        System.out.println("Kamusta Mundo!");
+    }
+}
+
+/** 
+ * Hello World Task
+ * clas helloWorldApp {
+    public static void main(String[] args) {
+        System.out.rintln("Hello World!); // Display the string.
+    }
+} */
