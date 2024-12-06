696aa3d0fa323d056990c2fabc444a0b55ebe913
KuroNoHikari <robert.delacruz.rdc27@gmail.com>
2024-12-06 11:40:23 +0000

    Output Done

---
+++ Primitives.java
@@ -0,0 +1,11 @@
+class Primitives {
+    public static void main(String[] args) {
+        char w = 'w', r = 'r', Di = 'd';
+        byte zero = 0;
+        int tree = 3;
+        short EL = 1;
+        float two = 2.0f;
+        String output = "H" + tree + EL + EL + zero + " " + w + zero + r + EL + Di + " " + two + " " + true;
+        System.out.println(output);
+    }
+}
