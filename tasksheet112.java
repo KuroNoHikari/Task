a0943c6fb893483dc6fa1d87ff5b22c1dc35d411
KuroNoHikari <robert.delacruz.rdc27@gmail.com>
2024-12-10 22:13:30 +0800

    Task Done

--- tasksheet112.java
+++ tasksheet112.java
@@ -1,13 +1,11 @@
-class tasksheet112 {
+public class tasksheet112 {
     public static void main(String[] args) {
-
-        int res,  a=1, b=2 , c=7 , d=8;
-        res = a+b;
-        System.out.println("Result is: " + res);
-        System.out.println("Result is: " + (res -=a));
-        System.out.println("Result is: " + (res *=b));
-        System.out.println("Result is: " + (res /=b));
-        res += d;
-        System.out.println("Result is: " + (res %= c));
+        int result = 1 + 2;
+        System.out.println(result);
+        System.out.println(result -=1);
+        System.out.println(result *=2);
+        System.out.println(result /=2);
+        System.out.println(result +=8);
+        System.out.println(result %=7);
     }
 }
